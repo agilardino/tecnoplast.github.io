730b2e123018672ad940fcd9f0016c75d027662f
agilardino <alejandro.gilardino@gmail.com>
2025-03-13 10:17:52 -0500

    title dinamico y elimniacion de assets

---
+++ js/dist/app.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var titles = {
+  "/index.html": "Inicio - TecnoPlast del Perú - Líder en soluciones plásticas",
+  "/nosotros.html": "Acerca de Nosotros - TecnoPlast del Perú - Líder en soluciones plásticas",
+  "/servicios.html": "Nuestros Servicios - TecnoPlast del Perú - Líder en soluciones plásticas",
+  "/productos.html": "Nuestros Productos - TecnoPlast del Perú - Líder en soluciones plásticas",
+  "/contacto.html": "Contáctanos - TecnoPlast del Perú - Líder en soluciones plásticas"
+}; // Cambia el título según la página actual
+
+document.title = titles[window.location.pathname] || "TecnoPlast del Perú - Líder en soluciones plásticas";
