60b0ceaacdfd1cdd0cdaa4ddc9335d5b056cdd6b
agilardino <alejandro.gilardino@gmail.com>
2025-03-13 16:55:38 -0500

    listo

--- js/app.js
+++ js/app.js
@@ -9,3 +9,36 @@ const titles = {
 // Cambia el título según la página actual
 document.title = titles[window.location.pathname] || "TecnoPlast del Perú - Líder en soluciones plásticas";
 
+// $(document).scroll(function() {
+//     var y = $(this).scrollTop();
+//     var windowHeight = $(window).height(); // Get window height
+  
+//     if (y > windowHeight / 10) {
+//       $(".chevron").hide();
+//     } else {
+//       $(".chevron").show();
+//     }
+//   });
+
+$(document).ready(function () {
+    function handleChevron() {
+      if (window.matchMedia("(max-width: 450px)").matches) {
+        $(document).scroll(function () {
+          var y = $(this).scrollTop();
+          var windowHeight = $(window).height();
+  
+          if (y > windowHeight / 10) {
+            $(".chevron").fadeOut();
+          } else {
+            $(".chevron").fadeIn();
+          }
+        });
+      } else {
+        $(".chevron").hide();
+      }
+    }
+  
+    handleChevron(); // Ejecutar al cargar la página
+    $(window).resize(handleChevron); // Volver a ejecutar al cambiar el tamaño de pantalla
+  });
+  
--- js/dist/app.dev.js
+++ js/dist/app.dev.js
@@ -8,4 +8,35 @@ var titles = {
   "/contacto.html": "Contáctanos - TecnoPlast del Perú - Líder en soluciones plásticas"
 }; // Cambia el título según la página actual
 
-document.title = titles[window.location.pathname] || "TecnoPlast del Perú - Líder en soluciones plásticas";
+document.title = titles[window.location.pathname] || "TecnoPlast del Perú - Líder en soluciones plásticas"; // $(document).scroll(function() {
+//     var y = $(this).scrollTop();
+//     var windowHeight = $(window).height(); // Get window height
+//     if (y > windowHeight / 10) {
+//       $(".chevron").hide();
+//     } else {
+//       $(".chevron").show();
+//     }
+//   });
+
+$(document).ready(function () {
+  function handleChevron() {
+    if (window.matchMedia("(max-width: 450px)").matches) {
+      $(document).scroll(function () {
+        var y = $(this).scrollTop();
+        var windowHeight = $(window).height();
+
+        if (y > windowHeight / 10) {
+          $(".chevron").fadeOut();
+        } else {
+          $(".chevron").fadeIn();
+        }
+      });
+    } else {
+      $(".chevron").hide();
+    }
+  }
+
+  handleChevron(); // Ejecutar al cargar la página
+
+  $(window).resize(handleChevron); // Volver a ejecutar al cambiar el tamaño de pantalla
+});
